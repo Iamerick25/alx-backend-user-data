cbf65a9d9d0e38da9ead08beba313893afbecf0c
Iamerick25 <erickchinyem@gmail.com>
2024-07-12 12:02:29 +0000

    Session cookie

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""
-Route module for the API
+""" Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
@@ -18,25 +17,56 @@ auth = None
 if os.getenv("AUTH_TYPE") == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif os.getenv("AUTH_TYPE") == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 else:
     from api.v1.auth.auth import Auth
     auth = Auth()
 
 
+""" Check the AUTH_TYPE """
+# Determine the authentication type based on the environment variable
+AUTH_TYPE = os.getenv("AUTH_TYPE")
+# Instantiate the appropriate authentication class
+if AUTH_TYPE == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+elif AUTH_TYPE == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif AUTH_TYPE == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+elif AUTH_TYPE == "session_exp_auth":
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+elif AUTH_TYPE == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
+
+
 @app.before_request
 def beforeRequest():
-    """
-    before request
+    """ Filter each request before it's handled by the proper route
+
+    Returns:
+        _type_: _description_
     """
     if auth is None:
-        return
-    paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
-    if not auth.require_auth(request.path, paths):
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
+        pass
+    else:
+        setattr(request, "current_user", auth.current_user(request))
+        excluded_list = ['/api/v1/status/',
+                         '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                         '/api/v1/auth_session/login/']
+
+        if auth.require_auth(request.path, excluded_list):
+            cookie = auth.session_cookie(request)
+            if auth.authorization_header(request) is None and cookie is None:
+                abort(401, description="Unauthorized")
+            if auth.current_user(request) is None:
+                abort(403, description='Forbidden')
 
 
 @app.errorhandler(404)
@@ -48,14 +78,14 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Unauthorized handler
+    """Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ forbidden handler
+    """Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
 
